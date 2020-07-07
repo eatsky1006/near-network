eatsky1006/near-network

commit 9dd1adea1624445503d7ca2a55917b0f0e1c01d8

fix(rpc): encode timestamp as string in BlockHeaderView (#2912)
Bridge testing is broken because of timestamp being too large (larger than u53) and therefore we cannot dump headers properly.

core/primitives/src/views.rs
@@ -25,7 +25,7 @@ use crate::receipt::{ActionReceipt, DataReceipt, DataReceiver, Receipt, ReceiptE
 use crate::rpc::RpcPagination;
 use crate::serialize::{
     base64_format, from_base64, option_base64_format, option_u128_dec_format, to_base64,
-    u128_dec_format,
+    u128_dec_format, u64_dec_format,
 };
 use crate::sharding::{ChunkHash, ShardChunk, ShardChunkHeader, ShardChunkHeaderInner};
 use crate::state_record::StateRecord;
@@ -328,6 +328,7 @@ pub struct BlockHeaderView {
     pub outcome_root: CryptoHash,
     pub chunks_included: u64,
     pub challenges_root: CryptoHash,
+    #[serde(with = "u64_dec_format")]
     pub timestamp: u64,
     pub random_value: CryptoHash,
     pub validator_proposals: Vec<ValidatorStakeView>,
@@ -441,6 +442,7 @@ pub struct BlockHeaderInnerLiteView {
     pub next_epoch_id: CryptoHash,
     pub prev_state_root: CryptoHash,
     pub outcome_root: CryptoHash,
+    #[serde(with = "u64_dec_format")]
     pub timestamp: u64,
     pub next_bp_hash: CryptoHash,
     pub block_merkle_root: CryptoHash,

core/primitives/src/serialize.rs
@@ -119,6 +119,26 @@ pub mod base_bytes_format {
     }
 }
 
+pub mod u64_dec_format {
+    use serde::de;
+    use serde::{Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S>(num: &u64, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&format!("{}", num))
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<u64, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        u64::from_str_radix(&s, 10).map_err(de::Error::custom)
+    }
+}
+
 pub mod u128_dec_format {
     use serde::de;
     use serde::{Deserialize, Deserializer, Serializer};

commit 94853425a9873db89315335cd39c52bbe8e014f0

fix(rpc): add timestamp_nanosec to keep backward compatibility (#2929)
Add `timestamp_nanosec` to keep backward compatibility on rpc side that was broken by #2912. Also fixes light client pytests that were broken because of the change.

chain/chain/src/lightclient.rs
@@ -43,6 +43,7 @@ pub fn create_light_client_block_view(
         prev_state_root: *block_header.prev_state_root(),
         outcome_root: *block_header.outcome_root(),
         timestamp: block_header.raw_timestamp(),
+        timestamp_nanosec: block_header.raw_timestamp(),
         next_bp_hash: *block_header.next_bp_hash(),
         block_merkle_root: *block_header.block_merkle_root(),
     };

core/primitives/src/views.rs
@@ -328,8 +328,10 @@ pub struct BlockHeaderView {
     pub outcome_root: CryptoHash,
     pub chunks_included: u64,
     pub challenges_root: CryptoHash,
-    #[serde(with = "u64_dec_format")]
+    /// Legacy json number. Should not be used.
     pub timestamp: u64,
+    #[serde(with = "u64_dec_format")]
+    pub timestamp_nanosec: u64,
     pub random_value: CryptoHash,
     pub validator_proposals: Vec<ValidatorStakeView>,
     pub chunk_mask: Vec<bool>,
@@ -369,6 +371,7 @@ impl From<BlockHeader> for BlockHeaderView {
             challenges_root: header.challenges_root().clone(),
             outcome_root: header.outcome_root().clone(),
             timestamp: header.raw_timestamp(),
+            timestamp_nanosec: header.raw_timestamp(),
             random_value: header.random_value().clone(),
             validator_proposals: header
                 .validator_proposals()
@@ -442,8 +445,10 @@ pub struct BlockHeaderInnerLiteView {
     pub next_epoch_id: CryptoHash,
     pub prev_state_root: CryptoHash,
     pub outcome_root: CryptoHash,
-    #[serde(with = "u64_dec_format")]
+    /// Legacy json number. Should not be used.
     pub timestamp: u64,
+    #[serde(with = "u64_dec_format")]
+    pub timestamp_nanosec: u64,
     pub next_bp_hash: CryptoHash,
     pub block_merkle_root: CryptoHash,
 }
@@ -458,6 +463,7 @@ impl From<BlockHeader> for BlockHeaderInnerLiteView {
                 prev_state_root: header.inner_lite.prev_state_root,
                 outcome_root: header.inner_lite.outcome_root,
                 timestamp: header.inner_lite.timestamp,
+                timestamp_nanosec: header.inner_lite.timestamp,
                 next_bp_hash: header.inner_lite.next_bp_hash,
                 block_merkle_root: header.inner_lite.block_merkle_root,
             },

pytest/lib/lightclient.py
@@ -42,7 +42,7 @@ def compute_block_hash(inner_lite_view, inner_rest_hash, prev_hash):
     inner_lite.prev_state_root = base58.b58decode(
         inner_lite_view['prev_state_root'])
     inner_lite.outcome_root = base58.b58decode(inner_lite_view['outcome_root'])
-    inner_lite.timestamp = inner_lite_view['timestamp']
+    inner_lite.timestamp = int(inner_lite_view['timestamp_nanosec'])
     inner_lite.next_bp_hash = base58.b58decode(inner_lite_view['next_bp_hash'])
     inner_lite.block_merkle_root = base58.b58decode(
         inner_lite_view['block_merkle_root'])