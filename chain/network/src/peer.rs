eatsky1006/near-network

commit b44616125fd1ac8c20fef1ccc765e8c04724db36

feat: Syncing chunks from archival nodes (#3302)
Currently we always request chunks parts either from a node to which the chunk part is assigned to, or from the chunk producer / full node tracking shard (if the chunk is assigned to us).

This works well with state sync, because statesync brings the node to a state as of epoch-two ago, and the block producers are all still online, but doesn't work well with the block sync from genesis, because for old blocks the block producers are almost certainly offline.

In this PR I make the following change:
1. If the chunk is from the current or the previous epoch, follow the logic we have today;
2. Otherwise, sync it from a peer that runs an archival node tracking the shard.

The change requires a change to the `Handshake`, so that archival nodes can report that they are archival.

Several seemingly unrelated changes that were necessary:
1. I changed the `num_data_parts` to be 1/3 of the total parts rounded up (instead of rounded down). In other words, it's `f+1` instead of `f` if `n=3f+1` is the total. Since the number of adversaries we assume is `f` or less, and the number of signatures on the block is `2f+1`, having `f+1` data parts doesn't break the data availability. Importantly, for four nodes it makes number of data parts 2, not 1, and thus the tests (including the tests for this change) actually trigger fetching chunk parts.

2. I'm changing the test infra to start proxy processes after the nodes are started, serially. Without that `Popen` sometimes hangs if the proxies for some processes happen to start before some other processes.

Test plan
---------

Adding two tests. `partial_chunk_messages` is not specific to this change, and makes sure that we do not send more requests (or messages) for chunks than we should. It's to prevent us accidentally introducing extra rounds of requests.

chain/network/src/peer.rs
@@ -21,7 +21,7 @@ use near_primitives::network::PeerId;
 use near_primitives::unwrap_option_or_return;
 use near_primitives::utils::DisplayOption;
 use near_primitives::version::{
-    ProtocolVersion, NETWORK_PROTOCOL_VERSION, OLDEST_BACKWARD_COMPATIBLE_PROTOCOL_VERSION,
+    ProtocolVersion, OLDEST_BACKWARD_COMPATIBLE_PROTOCOL_VERSION, PROTOCOL_VERSION,
 };
 
 use crate::codec::{self, bytes_to_peer_message, peer_message_to_bytes, Codec};
@@ -30,12 +30,12 @@ use crate::rate_counter::RateCounter;
 use crate::recorder::{PeerMessageMetadata, Status};
 use crate::routing::{Edge, EdgeInfo};
 use crate::types::{
-    Ban, Consolidate, ConsolidateResponse, HandshakeFailureReason, HandshakeV2,
+    Ban, Consolidate, ConsolidateResponse, Handshake, HandshakeFailureReason, HandshakeV2,
     NetworkClientMessages, NetworkClientResponses, NetworkRequests, NetworkViewClientMessages,
-    NetworkViewClientResponses, PeerChainInfo, PeerInfo, PeerManagerRequest, PeerMessage,
-    PeerRequest, PeerResponse, PeerStatsResult, PeerStatus, PeerType, PeersRequest, PeersResponse,
-    QueryPeerStats, ReasonForBan, RoutedMessage, RoutedMessageBody, RoutedMessageFrom, SendMessage,
-    Unregister, UPDATE_INTERVAL_LAST_TIME_RECEIVED_MESSAGE,
+    NetworkViewClientResponses, PeerChainInfo, PeerChainInfoV2, PeerInfo, PeerManagerRequest,
+    PeerMessage, PeerRequest, PeerResponse, PeerStatsResult, PeerStatus, PeerType, PeersRequest,
+    PeersResponse, QueryPeerStats, ReasonForBan, RoutedMessage, RoutedMessageBody,
+    RoutedMessageFrom, SendMessage, Unregister, UPDATE_INTERVAL_LAST_TIME_RECEIVED_MESSAGE,
 };
 use crate::PeerManagerActor;
 use crate::{metrics, NetworkResponses};
@@ -167,7 +167,7 @@ pub struct Peer {
     /// This node genesis id.
     genesis_id: GenesisId,
     /// Latest chain info from the peer.
-    chain_info: PeerChainInfo,
+    chain_info: PeerChainInfoV2,
     /// Edge information needed to build the real edge. This is relevant for handshake.
     edge_info: Option<EdgeInfo>,
     /// Last time an update of received message was sent to PeerManager
@@ -199,7 +199,7 @@ impl Peer {
             peer_info: peer_info.into(),
             peer_type,
             peer_status: PeerStatus::Connecting,
-            protocol_version: NETWORK_PROTOCOL_VERSION,
+            protocol_version: PROTOCOL_VERSION,
             framed,
             handshake_timeout,
             peer_manager_addr,
@@ -286,16 +286,32 @@ impl Peer {
                     genesis_id,
                     height,
                     tracked_shards,
+                    archival,
                 }) => {
-                    let handshake = HandshakeV2::new(
-                        act.protocol_version,
-                        act.node_id(),
-                        act.peer_id().unwrap(),
-                        act.node_info.addr_port(),
-                        PeerChainInfo { genesis_id, height, tracked_shards },
-                        act.edge_info.as_ref().unwrap().clone(),
-                    );
-                    act.send_message(PeerMessage::HandshakeV2(handshake));
+                    let handshake = match act.protocol_version {
+                        37..=PROTOCOL_VERSION => PeerMessage::Handshake(Handshake::new(
+                            act.protocol_version,
+                            act.node_id(),
+                            act.peer_id().unwrap(),
+                            act.node_info.addr_port(),
+                            PeerChainInfoV2 { genesis_id, height, tracked_shards, archival },
+                            act.edge_info.as_ref().unwrap().clone(),
+                        )),
+                        34..=36 => PeerMessage::HandshakeV2(HandshakeV2::new(
+                            act.protocol_version,
+                            act.node_id(),
+                            act.peer_id().unwrap(),
+                            act.node_info.addr_port(),
+                            PeerChainInfo { genesis_id, height, tracked_shards },
+                            act.edge_info.as_ref().unwrap().clone(),
+                        )),
+                        _ => {
+                            error!(target: "network", "Trying to talk with peer with no supported version: {}", act.protocol_version);
+                            return actix::fut::ready(());
+                        }
+                    };
+
+                    act.send_message(handshake);
                     actix::fut::ready(())
                 }
                 Err(err) => {
@@ -649,7 +665,7 @@ impl StreamHandler<Result<Vec<u8>, ReasonForBan>> for Peer {
                     self.send_message(PeerMessage::HandshakeFailure(
                         self.node_info.clone(),
                         HandshakeFailureReason::ProtocolVersionMismatch {
-                            version: NETWORK_PROTOCOL_VERSION,
+                            version: PROTOCOL_VERSION,
                             oldest_supported_version: OLDEST_BACKWARD_COMPATIBLE_PROTOCOL_VERSION,
                         },
                     ));
@@ -693,8 +709,8 @@ impl StreamHandler<Result<Vec<u8>, ReasonForBan>> for Peer {
             msg.len() as i64,
         );
 
-        if let PeerMessage::Handshake(handshake) = peer_msg {
-            peer_msg = PeerMessage::HandshakeV2(handshake.into());
+        if let PeerMessage::HandshakeV2(handshake) = peer_msg {
+            peer_msg = PeerMessage::Handshake(handshake.into());
         }
 
         match (self.peer_type, self.peer_status, peer_msg) {
@@ -707,7 +723,7 @@ impl StreamHandler<Result<Vec<u8>, ReasonForBan>> for Peer {
                         version,
                         oldest_supported_version,
                     } => {
-                        let target_version = std::cmp::min(version, NETWORK_PROTOCOL_VERSION);
+                        let target_version = std::cmp::min(version, PROTOCOL_VERSION);
 
                         if target_version
                             >= std::cmp::max(
@@ -720,7 +736,7 @@ impl StreamHandler<Result<Vec<u8>, ReasonForBan>> for Peer {
                             self.send_handshake(ctx);
                             return;
                         } else {
-                            warn!(target: "network", "Unable to connect to a node ({}) due to a network protocol version mismatch. Our version: {:?}, their: {:?}", peer_info, (NETWORK_PROTOCOL_VERSION, OLDEST_BACKWARD_COMPATIBLE_PROTOCOL_VERSION), (version, oldest_supported_version));
+                            warn!(target: "network", "Unable to connect to a node ({}) due to a network protocol version mismatch. Our version: {:?}, their: {:?}", peer_info, (PROTOCOL_VERSION, OLDEST_BACKWARD_COMPATIBLE_PROTOCOL_VERSION), (version, oldest_supported_version));
                         }
                     }
                     HandshakeFailureReason::InvalidTarget => {
@@ -730,12 +746,12 @@ impl StreamHandler<Result<Vec<u8>, ReasonForBan>> for Peer {
                 }
                 ctx.stop();
             }
-            (_, PeerStatus::Connecting, PeerMessage::HandshakeV2(handshake)) => {
+            (_, PeerStatus::Connecting, PeerMessage::Handshake(handshake)) => {
                 debug!(target: "network", "{:?}: Received handshake {:?}", self.node_info.id, handshake);
 
                 debug_assert!(
                     OLDEST_BACKWARD_COMPATIBLE_PROTOCOL_VERSION <= handshake.version
-                        && handshake.version <= NETWORK_PROTOCOL_VERSION
+                        && handshake.version <= PROTOCOL_VERSION
                 );
 
                 if handshake.chain_info.genesis_id != self.genesis_id {

chain/chain/src/test_utils.rs
@@ -906,6 +906,14 @@ impl RuntimeAdapter for KeyValueRuntime {
         }
     }
 
+    fn chunk_needs_to_be_fetched_from_archival(
+        &self,
+        _chunk_prev_block_hash: &CryptoHash,
+        _header_head: &CryptoHash,
+    ) -> Result<bool, Error> {
+        Ok(false)
+    }
+
     fn verify_validator_or_fisherman_signature(
         &self,
         _epoch_id: &EpochId,

chain/chain/src/types.rs
@@ -556,6 +556,12 @@ pub trait RuntimeAdapter: Send + Sync {
         other_epoch_id: &EpochId,
     ) -> Result<Ordering, Error>;
 
+    fn chunk_needs_to_be_fetched_from_archival(
+        &self,
+        chunk_prev_block_hash: &CryptoHash,
+        header_head: &CryptoHash,
+    ) -> Result<bool, Error>;
+
     /// Build receipts hashes.
     // Due to borsh serialization constraints, we have to use `&Vec<Receipt>` instead of `&[Receipt]`
     // here.

chain/chunks/src/lib.rs
@@ -14,7 +14,8 @@ use near_chain::{
     byzantine_assert, ChainStore, ChainStoreAccess, ChainStoreUpdate, ErrorKind, RuntimeAdapter,
 };
 use near_network::types::{
-    NetworkAdapter, PartialEncodedChunkRequestMsg, PartialEncodedChunkResponseMsg,
+    AccountIdOrPeerTrackingShard, NetworkAdapter, PartialEncodedChunkRequestMsg,
+    PartialEncodedChunkResponseMsg,
 };
 use near_network::NetworkRequests;
 use near_pool::{PoolIteratorWrapper, TransactionPool};
@@ -434,6 +435,7 @@ impl ShardsManager {
         chunk_hash: &ChunkHash,
         force_request_full: bool,
         request_own_parts_from_others: bool,
+        request_from_archival: bool,
     ) -> Result<(), near_chain::Error> {
         let mut bp_to_parts = HashMap::new();
 
@@ -453,14 +455,21 @@ impl ShardsManager {
             shard_id,
         )?;
 
-        let shard_representative_account_id = if !request_own_parts_from_others {
-            chunk_producer_account_id.clone()
-        } else {
-            match self.get_random_shard_block_producer(&parent_hash, shard_id) {
-                Ok(Some(someone)) => someone,
-                Ok(None) | Err(_) => chunk_producer_account_id.clone(),
-            }
-        };
+        let shard_representative_target =
+            if !request_own_parts_from_others && !request_from_archival {
+                AccountIdOrPeerTrackingShard::AccountId(chunk_producer_account_id.clone())
+            } else {
+                match self.get_random_target_tracking_shard(
+                    &parent_hash,
+                    shard_id,
+                    request_from_archival,
+                ) {
+                    Ok(Some(someone)) => someone,
+                    Ok(None) | Err(_) => {
+                        AccountIdOrPeerTrackingShard::AccountId(chunk_producer_account_id.clone())
+                    }
+                }
+            };
 
         let seal = self.seals_mgr.get_seal(chunk_hash, parent_hash, height, shard_id)?;
 
@@ -481,12 +490,17 @@ impl ShardsManager {
             };
 
             if need_to_fetch_part {
-                let fetch_from = self.runtime_adapter.get_part_owner(&parent_hash, part_ord)?;
-                let fetch_from = if Some(&fetch_from) == self.me.as_ref() {
-                    // If missing own part, request it from the chunk producer
-                    shard_representative_account_id.clone()
+                let fetch_from = if request_from_archival {
+                    shard_representative_target.clone()
                 } else {
-                    fetch_from
+                    let part_owner = self.runtime_adapter.get_part_owner(&parent_hash, part_ord)?;
+
+                    if Some(&part_owner) == self.me.as_ref() {
+                        // If missing own part, request it from the chunk producer / node tracking shard
+                        shard_representative_target.clone()
+                    } else {
+                        AccountIdOrPeerTrackingShard::AccountId(part_owner)
+                    }
                 };
 
                 bp_to_parts.entry(fetch_from).or_insert_with(|| vec![]).push(part_ord);
@@ -502,29 +516,31 @@ impl ShardsManager {
         //     for some subset of shards, even if we don't need to request any parts from the original
         //     chunk producer.
         if !shards_to_fetch_receipts.is_empty() {
-            bp_to_parts.entry(shard_representative_account_id.clone()).or_insert_with(|| vec![]);
+            bp_to_parts.entry(shard_representative_target.clone()).or_insert_with(|| vec![]);
         }
 
-        for (account_id, part_ords) in bp_to_parts {
+        for (target, part_ords) in bp_to_parts {
             // extra check that we are not sending request to ourselves.
-            if Some(&account_id) != self.me.as_ref() {
+            if self
+                .me
+                .clone()
+                .map_or(true, |me| AccountIdOrPeerTrackingShard::AccountId(me) != target)
+            {
                 let request = PartialEncodedChunkRequestMsg {
                     chunk_hash: chunk_hash.clone(),
                     part_ords,
-                    tracking_shards: if account_id == shard_representative_account_id {
+                    tracking_shards: if target == shard_representative_target {
                         shards_to_fetch_receipts.clone()
                     } else {
                         HashSet::new()
                     },
                 };
 
-                self.network_adapter.do_send(NetworkRequests::PartialEncodedChunkRequest {
-                    account_id: account_id.clone(),
-                    request,
-                });
+                self.network_adapter
+                    .do_send(NetworkRequests::PartialEncodedChunkRequest { target, request });
             } else {
                 warn!(target: "client", "{} requests parts {:?} for chunk {:?} from self",
-                    account_id, part_ords, chunk_hash
+                    self.me.clone().unwrap(), part_ords, chunk_hash
                 );
             }
         }
@@ -533,11 +549,12 @@ impl ShardsManager {
     }
 
     /// Get a random shard block producer that is not me.
-    fn get_random_shard_block_producer(
+    fn get_random_target_tracking_shard(
         &self,
         parent_hash: &CryptoHash,
         shard_id: ShardId,
-    ) -> Result<Option<AccountId>, Error> {
+        request_from_archival: bool,
+    ) -> Result<Option<AccountIdOrPeerTrackingShard>, Error> {
         let mut block_producers = vec![];
         let epoch_id = self.runtime_adapter.get_epoch_id_from_prev_block(parent_hash).unwrap();
         for (validator_stake, is_slashed) in
@@ -556,7 +573,17 @@ impl ShardsManager {
             }
         }
 
-        Ok(block_producers.choose(&mut rand::thread_rng()).cloned())
+        let maybe_account_id = block_producers.choose(&mut rand::thread_rng()).cloned();
+
+        Ok(if request_from_archival {
+            Some(AccountIdOrPeerTrackingShard::PeerTrackingShard {
+                shard_id,
+                only_archival: true,
+                fallback_account_id: maybe_account_id,
+            })
+        } else {
+            maybe_account_id.map(|x| AccountIdOrPeerTrackingShard::AccountId(x))
+        })
     }
 
     fn get_tracking_shards(&self, parent_hash: &CryptoHash) -> HashSet<ShardId> {
@@ -572,7 +599,7 @@ impl ShardsManager {
             .collect::<HashSet<_>>()
     }
 
-    pub fn request_chunks<T>(&mut self, chunks_to_request: T)
+    pub fn request_chunks<T>(&mut self, chunks_to_request: T, header_head: &CryptoHash)
     where
         T: IntoIterator<Item = ShardChunkHeader>,
     {
@@ -605,6 +632,13 @@ impl ShardsManager {
                     added: Instant::now(),
                 },
             );
+
+            let fetch_from_archival = self.runtime_adapter
+                .chunk_needs_to_be_fetched_from_archival(&parent_hash, header_head).unwrap_or_else(|err| {
+                error!(target: "chunks", "Error during requesting partial encoded chunk. Cannot determine whether to request from an archival node, defaulting to not: {}", err);
+                false
+            });
+
             let request_result = self.request_partial_encoded_chunk(
                 height,
                 &parent_hash,
@@ -612,6 +646,7 @@ impl ShardsManager {
                 &chunk_hash,
                 false,
                 false,
+                fetch_from_archival,
             );
             if let Err(err) = request_result {
                 error!(target: "chunks", "Error during requesting partial encoded chunk: {}", err);
@@ -620,10 +655,15 @@ impl ShardsManager {
     }
 
     /// Resends chunk requests if haven't received it within expected time.
-    pub fn resend_chunk_requests(&mut self) {
+    pub fn resend_chunk_requests(&mut self, header_head: &CryptoHash) {
         // Process chunk one part requests.
         let requests = self.requested_partial_encoded_chunks.fetch();
         for (chunk_hash, chunk_request) in requests {
+            let fetch_from_archival = self.runtime_adapter
+                .chunk_needs_to_be_fetched_from_archival(&chunk_request.parent_hash, header_head).unwrap_or_else(|err| {
+                error!(target: "chunks", "Error during re-requesting partial encoded chunk. Cannot determine whether to request from an archival node, defaulting to not: {}", err);
+                false
+            });
             match self.request_partial_encoded_chunk(
                 chunk_request.height,
                 &chunk_request.parent_hash,
@@ -633,6 +673,7 @@ impl ShardsManager {
                     > self.requested_partial_encoded_chunks.switch_to_full_fetch_duration,
                 chunk_request.added.elapsed()
                     > self.requested_partial_encoded_chunks.switch_to_others_duration,
+                fetch_from_archival,
             ) {
                 Ok(()) => {}
                 Err(err) => {
@@ -1507,7 +1548,7 @@ mod test {
             },
         );
         std::thread::sleep(Duration::from_millis(2 * CHUNK_REQUEST_RETRY_MS));
-        shards_manager.resend_chunk_requests();
+        shards_manager.resend_chunk_requests(&Default::default());
         assert!(network_adapter.requests.read().unwrap().is_empty());
     }
 
@@ -1571,6 +1612,7 @@ mod test {
                 &encoded_chunk.header.inner.prev_block_hash,
                 encoded_chunk.header.inner.shard_id,
                 &encoded_chunk.header.hash,
+                false,
                 false,
                 false,
             )

chain/client/src/client.rs
@@ -681,6 +681,11 @@ impl Client {
                 .unwrap()
                 .drain(..)
                 .flat_map(|missing_chunks| missing_chunks.into_iter()),
+            &self
+                .chain
+                .header_head()
+                .expect("header_head must be available when processing a block")
+                .last_block_hash,
         );
 
         let unwrapped_accepted_blocks = accepted_blocks.write().unwrap().drain(..).collect();
@@ -719,7 +724,10 @@ impl Client {
                 Ok(self.process_blocks_with_missing_chunks(prev_block_hash))
             }
             ProcessPartialEncodedChunkResult::NeedMorePartsOrReceipts(chunk_header) => {
-                self.shards_mgr.request_chunks(iter::once(*chunk_header));
+                self.shards_mgr.request_chunks(
+                    iter::once(*chunk_header),
+                    &self.chain.header_head()?.last_block_hash,
+                );
                 Ok(vec![])
             }
             ProcessPartialEncodedChunkResult::NeedBlock => {
@@ -991,6 +999,11 @@ impl Client {
                 .unwrap()
                 .drain(..)
                 .flat_map(|missing_chunks| missing_chunks.into_iter()),
+            &self
+                .chain
+                .header_head()
+                .expect("header_head must be avaiable when processing blocks with missing chunks")
+                .last_block_hash,
         );
 
         let unwrapped_accepted_blocks = accepted_blocks.write().unwrap().drain(..).collect();
@@ -1391,6 +1404,7 @@ impl Client {
                             .unwrap()
                             .drain(..)
                             .flat_map(|missing_chunks| missing_chunks.into_iter()),
+                        &self.chain.header_head()?.last_block_hash,
                     );
 
                     let unwrapped_accepted_blocks =

chain/client/src/client_actor.rs
@@ -768,7 +768,11 @@ impl ClientActor {
             self.client.config.chunk_request_retry_period,
             self.chunk_request_retry_next_attempt,
             ctx,
-            |act, _ctx| act.client.shards_mgr.resend_chunk_requests(),
+            |act, _ctx| {
+                if let Ok(header_head) = act.client.chain.header_head() {
+                    act.client.shards_mgr.resend_chunk_requests(&header_head.last_block_hash)
+                }
+            },
         );
         core::cmp::min(
             delay,
@@ -829,7 +833,10 @@ impl ClientActor {
                                 missing_chunks,
                                 missing_chunks.iter().map(|header| header.chunk_hash()).collect::<Vec<_>>()
                             );
-                            self.client.shards_mgr.request_chunks(missing_chunks);
+                            self.client.shards_mgr.request_chunks(
+                                missing_chunks,
+                                &self.client.chain.header_head().expect("header_head must be available when processing newly produced block").last_block_hash,
+                            );
                             Ok(())
                         }
                         _ => {
@@ -945,7 +952,15 @@ impl ClientActor {
                         missing_chunks,
                         missing_chunks.iter().map(|header| header.chunk_hash()).collect::<Vec<_>>()
                     );
-                    self.client.shards_mgr.request_chunks(missing_chunks);
+                    self.client.shards_mgr.request_chunks(
+                        missing_chunks,
+                        &self
+                            .client
+                            .chain
+                            .header_head()
+                            .expect("header_head should always be available when block is received")
+                            .last_block_hash,
+                    );
                 }
                 _ => {
                     debug!(target: "client", "Process block: block {} refused by chain: {}", hash, e.kind());
@@ -1266,6 +1281,12 @@ impl ClientActor {
                                 .unwrap()
                                 .drain(..)
                                 .flat_map(|missing_chunks| missing_chunks.into_iter()),
+                            &self
+                                .client
+                                .chain
+                                .header_head()
+                                .expect("header_head must be available during sync")
+                                .last_block_hash,
                         );
 
                         self.client.sync_status =

chain/client/src/sync.rs
@@ -1174,7 +1174,7 @@ mod test {
     use near_crypto::{KeyType, PublicKey};
     use near_network::routing::EdgeInfo;
     use near_network::test_utils::MockNetworkAdapter;
-    use near_network::types::PeerChainInfo;
+    use near_network::types::PeerChainInfoV2;
     use near_network::PeerInfo;
     use near_primitives::block::{Approval, Block, GenesisId};
     use near_primitives::network::PeerId;
@@ -1239,13 +1239,14 @@ mod test {
         let mut sync_status = SyncStatus::NoSync;
         let peer1 = FullPeerInfo {
             peer_info: PeerInfo::random(),
-            chain_info: PeerChainInfo {
+            chain_info: PeerChainInfoV2 {
                 genesis_id: GenesisId {
                     chain_id: "unittest".to_string(),
                     hash: *chain.genesis().hash(),
                 },
                 height: chain2.head().unwrap().height,
                 tracked_shards: vec![],
+                archival: false,
             },
             edge_info: EdgeInfo::default(),
         };

commit 2e0abe6db658e4b3c46dc8ee4d67323dbb0f1ebd

fix: handshake versioning (#3417)
When #3302 started the protocol version was 37 but when it is merged, the protocol version got bumped to 39 and therefore this line https://github.com/nearprotocol/nearcore/blob/6efb708f138ee3858b7ed3c4613c055282a26371/chain/network/src/peer.rs#L292 is inconsistent with the new protocol version introduced in #3302. Currently it only works because node before this change always send `HandShakeV2`, which the new node is able to deserialize. Otherwise a node running current master would not be able to connect to a node on protocol version 38 (current testnet).

chain/network/src/peer.rs
@@ -289,7 +289,7 @@ impl Peer {
                     archival,
                 }) => {
                     let handshake = match act.protocol_version {
-                        37..=PROTOCOL_VERSION => PeerMessage::Handshake(Handshake::new(
+                        39..=PROTOCOL_VERSION => PeerMessage::Handshake(Handshake::new(
                             act.protocol_version,
                             act.node_id(),
                             act.peer_id().unwrap(),
@@ -297,7 +297,7 @@ impl Peer {
                             PeerChainInfoV2 { genesis_id, height, tracked_shards, archival },
                             act.edge_info.as_ref().unwrap().clone(),
                         )),
-                        34..=36 => PeerMessage::HandshakeV2(HandshakeV2::new(
+                        34..=38 => PeerMessage::HandshakeV2(HandshakeV2::new(
                             act.protocol_version,
                             act.node_id(),
                             act.peer_id().unwrap(),