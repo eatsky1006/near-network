near/nearcore

commit 8e0bdf35153bb830521788451a57585ca6263573

[stateless_validation] Add a soft size limit for state witness (#10703)
This PR adds a new runtime config `state_witness_size_soft_limit` with
size about 16 MB to begin with along with an implementation to enforce
it in runtime.

This is the first step of https://github.com/near/nearcore/issues/10259

What is state witness size soft limit?

In order to limit the size of the state witness, as a first step, we are
adding a limit to the max size of the state witness partial trie or
proof. In runtime, we record all the trie nodes touched by the chunk
execution and include these in the state witness. With the limit in
place, if the size of the state witness exceeds 16 MB, then we would
stop applying receipts further and push all receipts into the delayed
queue.

The reason we call this a soft limit is that we stop the execution of
the receipts AFTER the size of the state witness has exceeded 16 MB.

We are including this as part of a new protocol version 83

Future steps
- Introduce limits on other parts of the state witness like new
transactions
- Introduce a hard size limit for individual contract executions
- Monitor size of state witness
- Add metrics in a separate PR

core/parameters/src/view.rs
@@ -40,6 +40,9 @@ pub struct RuntimeFeesConfigView {
 
     /// Pessimistic gas price inflation ratio.
     pub pessimistic_gas_price_inflation_ratio: Rational32,
+
+    /// The maximum size of the state witness after which we defer execution of any new receipts.
+    pub storage_proof_size_soft_limit: usize,
 }
 
 /// The structure describes configuration for creation of new accounts.
@@ -179,6 +182,7 @@ impl From<crate::RuntimeConfig> for RuntimeConfigView {
                 pessimistic_gas_price_inflation_ratio: config
                     .fees
                     .pessimistic_gas_price_inflation_ratio,
+                storage_proof_size_soft_limit: config.storage_proof_size_soft_limit,
             },
             wasm_config: VMConfigView::from(config.wasm_config),
             account_creation_config: AccountCreationConfigView {
@@ -612,6 +616,7 @@ mod tests {
     /// fields, only adding fields is allowed or we risk breaking clients.
     #[test]
     #[cfg_attr(feature = "nightly", ignore)]
+    #[cfg_attr(feature = "statelessnet_protocol", ignore)]
     fn test_runtime_config_view() {
         use crate::view::RuntimeConfigView;
         use crate::RuntimeConfig;

core/parameters/res/runtime_configs/83.yaml
@@ -1,0 +1,1 @@
+storage_proof_size_soft_limit: {old: 999_999_999_999_999, new: 16_000_000}

core/parameters/res/runtime_configs/parameters.snap
@@ -4,6 +4,7 @@ description: THIS IS AN AUTOGENERATED FILE. DO NOT EDIT THIS FILE DIRECTLY.
 ---
 burnt_gas_reward                        3 / 10
 pessimistic_gas_price_inflation         103 / 100
+storage_proof_size_soft_limit            999_999_999_999_999
 min_allowed_top_level_account_length                      65
 registrar_account_id                    registrar
 storage_amount_per_byte                 10000000000000000000
@@ -180,4 +181,3 @@ function_call_weight                    true
 vm_kind                                 NearVm
 eth_implicit_accounts                   false
 yield_resume                            false
-

core/parameters/res/runtime_configs/parameters.yaml
@@ -12,6 +12,9 @@ pessimistic_gas_price_inflation: {
   denominator: 100,
 }
 
+# Stateless validation config
+storage_proof_size_soft_limit: 999_999_999_999_999
+
 # Account creation config
 min_allowed_top_level_account_length: 32
 registrar_account_id: "registrar"

core/parameters/res/runtime_configs/parameters_testnet.yaml
@@ -8,6 +8,9 @@ pessimistic_gas_price_inflation: {
   denominator: 100,
 }
 
+# Stateless validation config
+storage_proof_size_soft_limit: 999_999_999_999_999
+
 # Account creation config
 min_allowed_top_level_account_length: 0
 registrar_account_id: "registrar"

core/parameters/src/config.rs
@@ -23,6 +23,8 @@ pub struct RuntimeConfig {
     pub wasm_config: crate::vm::Config,
     /// Config that defines rules for account creation.
     pub account_creation_config: AccountCreationConfig,
+    /// The maximum size of the storage proof in state witness after which we defer execution of any new receipts.
+    pub storage_proof_size_soft_limit: usize,
 }
 
 impl RuntimeConfig {
@@ -45,6 +47,7 @@ impl RuntimeConfig {
             fees: RuntimeFeesConfig::test(),
             wasm_config,
             account_creation_config: AccountCreationConfig::default(),
+            storage_proof_size_soft_limit: usize::MAX,
         }
     }
 
@@ -57,6 +60,7 @@ impl RuntimeConfig {
             fees: RuntimeFeesConfig::free(),
             wasm_config,
             account_creation_config: AccountCreationConfig::default(),
+            storage_proof_size_soft_limit: usize::MAX,
         }
     }
 

core/parameters/src/config_store.rs
@@ -37,6 +37,7 @@ static CONFIG_DIFFS: &[(ProtocolVersion, &str)] = &[
     (62, include_config!("62.yaml")),
     (63, include_config!("63.yaml")),
     (64, include_config!("64.yaml")),
+    (83, include_config!("83.yaml")),
     (129, include_config!("129.yaml")),
     // Introduce ETH-implicit accounts.
     (138, include_config!("138.yaml")),
@@ -317,6 +318,7 @@ mod tests {
     /// the new snapshot if it looks right.
     #[test]
     #[cfg(not(feature = "nightly"))]
+    #[cfg(not(feature = "statelessnet_protocol"))]
     #[cfg(not(feature = "calimero_zero_storage"))]
     fn test_json_unchanged() {
         use crate::view::RuntimeConfigView;

core/parameters/src/parameter.rs
@@ -20,6 +20,9 @@ pub enum Parameter {
     BurntGasReward,
     PessimisticGasPriceInflation,
 
+    // Stateless validation config
+    StorageProofSizeSoftLimit,
+
     // Account creation config
     MinAllowedTopLevelAccountLength,
     RegistrarAccountId,

eatsky1006/near-network

commit 72ec95e24b6df7007e661f1de14e7f991817f2be

[fix] Fix Linux StatelessNet Github CI (#10731)
Runtime config check related tests are not supposed to run on nightly
and statelessnet.

Originally broken in https://github.com/near/nearcore/pull/10703

core/parameters/Cargo.toml
@@ -39,4 +39,7 @@ nightly = [
 nightly_protocol = [
   "near-primitives-core/nightly_protocol",
 ]
+statelessnet_protocol = [
+  "near-primitives-core/statelessnet_protocol",
+]
 calimero_zero_storage = []

core/parameters/src/view.rs
@@ -611,12 +611,12 @@ impl From<ExtCostsConfigView> for crate::ExtCostsConfig {
 }
 
 #[cfg(test)]
+#[cfg(not(feature = "nightly"))]
+#[cfg(not(feature = "statelessnet_protocol"))]
 mod tests {
     /// The JSON representation used in RPC responses must not remove or rename
     /// fields, only adding fields is allowed or we risk breaking clients.
     #[test]
-    #[cfg_attr(feature = "nightly", ignore)]
-    #[cfg_attr(feature = "statelessnet_protocol", ignore)]
     fn test_runtime_config_view() {
         use crate::view::RuntimeConfigView;
         use crate::RuntimeConfig;