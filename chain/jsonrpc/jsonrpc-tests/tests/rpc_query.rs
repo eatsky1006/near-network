eatsky1006/near-network

commit 0b9ce692225de3960a7903d9e4379b08e071ee17

feat: improved RPC's response error handling with HTTP status codes (#11806)
This PR updates the RPC handler to return appropriate HTTP errors for
specific RPC issues, improving error visibility for operators

closes #11792

---------

Co-authored-by: Shreyan Gupta <shreyan.gupta96@gmail.com>

chain/jsonrpc/src/lib.rs
@@ -6,8 +6,6 @@ use actix_web::HttpRequest;
 use actix_web::{get, http, middleware, web, App, Error as HttpError, HttpResponse, HttpServer};
 use api::RpcRequest;
 pub use api::{RpcFrom, RpcInto};
-use futures::Future;
-use futures::FutureExt;
 use near_async::actix::ActixResult;
 use near_async::messaging::{
     AsyncSendError, AsyncSender, CanSend, MessageWithCallback, SendAsync, Sender,
@@ -21,7 +19,7 @@ use near_client::{
 };
 use near_client_primitives::types::GetSplitStorageInfo;
 pub use near_jsonrpc_client as client;
-use near_jsonrpc_primitives::errors::RpcError;
+use near_jsonrpc_primitives::errors::{RpcError, RpcErrorKind};
 use near_jsonrpc_primitives::message::{Message, Request};
 use near_jsonrpc_primitives::types::config::{RpcProtocolConfigError, RpcProtocolConfigResponse};
 use near_jsonrpc_primitives::types::entity_debug::{EntityDebugHandler, EntityQuery};
@@ -288,15 +286,13 @@ struct JsonRpcHandler {
 }
 
 impl JsonRpcHandler {
-    pub async fn process(&self, message: Message) -> Result<Message, HttpError> {
+    async fn process(&self, message: Message) -> Message {
         let id = message.id();
         match message {
-            Message::Request(request) => {
-                Ok(Message::response(id, self.process_request(request).await))
-            }
-            _ => Ok(Message::error(RpcError::parse_error(
+            Message::Request(request) => Message::response(id, self.process_request(request).await),
+            _ => Message::error(RpcError::parse_error(
                 "JSON RPC Request format was expected".to_owned(),
-            ))),
+            )),
         }
     }
 
@@ -1355,29 +1351,34 @@ impl JsonRpcHandler {
     }
 }
 
-fn rpc_handler(
+async fn rpc_handler(
     message: web::Json<Message>,
     handler: web::Data<JsonRpcHandler>,
-) -> impl Future<Output = Result<HttpResponse, HttpError>> {
-    let response = async move {
-        let message = handler.process(message.0).await?;
-        Ok(HttpResponse::Ok().json(&message))
+) -> HttpResponse {
+    let message = handler.process(message.0).await;
+    let mut response = if let Message::Response(response) = &message {
+        match &response.result {
+            Ok(_) => HttpResponse::Ok(),
+            Err(err) => match err.error_struct {
+                Some(RpcErrorKind::InternalError(_)) | Some(RpcErrorKind::HandlerError(_)) => {
+                    HttpResponse::InternalServerError()
+                }
+                _ => HttpResponse::Ok(),
+            },
+        }
+    } else {
+        HttpResponse::InternalServerError()
     };
-    response.boxed()
+    response.json(message)
 }
 
-fn status_handler(
-    handler: web::Data<JsonRpcHandler>,
-) -> impl Future<Output = Result<HttpResponse, HttpError>> {
+async fn status_handler(handler: web::Data<JsonRpcHandler>) -> Result<HttpResponse, HttpError> {
     metrics::HTTP_STATUS_REQUEST_COUNT.inc();
 
-    let response = async move {
-        match handler.status().await {
-            Ok(value) => Ok(HttpResponse::Ok().json(&value)),
-            Err(_) => Ok(HttpResponse::ServiceUnavailable().finish()),
-        }
-    };
-    response.boxed()
+    match handler.status().await {
+        Ok(value) => Ok(HttpResponse::Ok().json(&value)),
+        Err(_) => Ok(HttpResponse::ServiceUnavailable().finish()),
+    }
 }
 
 async fn debug_handler(
@@ -1420,28 +1421,20 @@ async fn debug_block_status_handler(
     }
 }
 
-fn health_handler(
-    handler: web::Data<JsonRpcHandler>,
-) -> impl Future<Output = Result<HttpResponse, HttpError>> {
-    let response = async move {
-        match handler.health().await {
-            Ok(value) => Ok(HttpResponse::Ok().json(&value)),
-            Err(_) => Ok(HttpResponse::ServiceUnavailable().finish()),
-        }
-    };
-    response.boxed()
+async fn health_handler(handler: web::Data<JsonRpcHandler>) -> Result<HttpResponse, HttpError> {
+    match handler.health().await {
+        Ok(value) => Ok(HttpResponse::Ok().json(&value)),
+        Err(_) => Ok(HttpResponse::ServiceUnavailable().finish()),
+    }
 }
 
-fn network_info_handler(
+async fn network_info_handler(
     handler: web::Data<JsonRpcHandler>,
-) -> impl Future<Output = Result<HttpResponse, HttpError>> {
-    let response = async move {
-        match handler.network_info().await {
-            Ok(value) => Ok(HttpResponse::Ok().json(&value)),
-            Err(_) => Ok(HttpResponse::ServiceUnavailable().finish()),
-        }
-    };
-    response.boxed()
+) -> Result<HttpResponse, HttpError> {
+    match handler.network_info().await {
+        Ok(value) => Ok(HttpResponse::Ok().json(&value)),
+        Err(_) => Ok(HttpResponse::ServiceUnavailable().finish()),
+    }
 }
 
 pub async fn prometheus_handler() -> Result<HttpResponse, HttpError> {
@@ -1457,16 +1450,13 @@ pub async fn prometheus_handler() -> Result<HttpResponse, HttpError> {
     }
 }
 
-fn client_config_handler(
+async fn client_config_handler(
     handler: web::Data<JsonRpcHandler>,
-) -> impl Future<Output = Result<HttpResponse, HttpError>> {
-    let response = async move {
-        match handler.client_config().await {
-            Ok(value) => Ok(HttpResponse::Ok().json(&value)),
-            Err(_) => Ok(HttpResponse::ServiceUnavailable().finish()),
-        }
-    };
-    response.boxed()
+) -> Result<HttpResponse, HttpError> {
+    match handler.client_config().await {
+        Ok(value) => Ok(HttpResponse::Ok().json(&value)),
+        Err(_) => Ok(HttpResponse::ServiceUnavailable().finish()),
+    }
 }
 
 fn get_cors(cors_allowed_origins: &[String]) -> Cors {

commit 15acf8befc4b0cf592744a8afec9d1529759dd8c

feat: handle `RpcError` properly (#11822)
This PR fixes a small issue that was introduced in #11806

Internal error - 500 status code
Handler error - 408 if it's a `TIMEOUT_ERROR`, otherwise 200
Request validation error - 400 status code

closes #11792

chain/jsonrpc/src/lib.rs
@@ -1359,11 +1359,17 @@ async fn rpc_handler(
     let mut response = if let Message::Response(response) = &message {
         match &response.result {
             Ok(_) => HttpResponse::Ok(),
-            Err(err) => match err.error_struct {
-                Some(RpcErrorKind::InternalError(_)) | Some(RpcErrorKind::HandlerError(_)) => {
-                    HttpResponse::InternalServerError()
+            Err(err) => match &err.error_struct {
+                Some(RpcErrorKind::RequestValidationError(_)) => HttpResponse::BadRequest(),
+                Some(RpcErrorKind::HandlerError(error_struct)) => {
+                    if error_struct["name"] == "TIMEOUT_ERROR" {
+                        HttpResponse::RequestTimeout()
+                    } else {
+                        HttpResponse::Ok()
+                    }
                 }
-                _ => HttpResponse::Ok(),
+                Some(RpcErrorKind::InternalError(_)) => HttpResponse::InternalServerError(),
+                None => HttpResponse::Ok(),
             },
         }
     } else {

pytest/lib/cluster.py
@@ -5,7 +5,7 @@ import json
 import os
 import pathlib
 import rc
-from geventhttpclient import Session
+from geventhttpclient import Session, useragent
 import shutil
 import signal
 import subprocess
@@ -434,17 +434,20 @@ class BaseNode(object):
 
     def check_store(self):
         if self.is_check_store:
-            res = self.json_rpc('adv_check_store', [])
-            if not 'result' in res:
-                # cannot check Storage Consistency for the node, possibly not Adversarial Mode is running
+            try:
+                res = self.json_rpc('adv_check_store', [])
+                if not 'result' in res:
+                    # cannot check Storage Consistency for the node, possibly not Adversarial Mode is running
+                    pass
+                else:
+                    if res['result'] == 0:
+                        logger.error(
+                            "Storage for %s:%s in inconsistent state, stopping"
+                            % self.addr())
+                        self.kill()
+                    self.store_tests += res['result']
+            except useragent.BadStatusCode:
                 pass
-            else:
-                if res['result'] == 0:
-                    logger.error(
-                        "Storage for %s:%s in inconsistent state, stopping" %
-                        self.addr())
-                    self.kill()
-                self.store_tests += res['result']
 
 
 class RpcNode(BaseNode):

chain/jsonrpc/jsonrpc-tests/tests/rpc_query.rs
@@ -2,6 +2,7 @@ use std::ops::ControlFlow;
 use std::str::FromStr;
 
 use actix::System;
+use awc::http::StatusCode;
 use futures::{future, FutureExt};
 use serde_json::json;
 
@@ -534,6 +535,8 @@ fn test_invalid_methods() {
                 .await
                 .unwrap();
 
+            assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+
             let response =
                 serde_json::from_value::<serde_json::Value>(response.json().await.unwrap())
                     .unwrap();
@@ -544,6 +547,78 @@ fn test_invalid_methods() {
                 method_name
             );
         }
+    });
+}
+
+#[test]
+fn test_parse_error_status_code() {
+    test_with_client!(test_utils::NodeType::NonValidator, client, async move {
+        let json = serde_json::json!({
+            "jsonrpc": "2.0",
+            "id": "dontcare",
+            "method": "tx",
+            "params": serde_json::json!({
+                "tx": "badtx",
+                "sender_account_id": "frolik.near"
+            }),
+        });
+
+        let response = &mut client
+            .client
+            .post(&client.server_addr)
+            .insert_header(("Content-Type", "application/json"))
+            .send_json(&json)
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::BAD_REQUEST);
+    });
+}
+
+#[test]
+fn test_bad_handler_error_status_code() {
+    test_with_client!(test_utils::NodeType::NonValidator, client, async move {
+        let json = serde_json::json!({
+            "jsonrpc": "2.0",
+            "id": "dontcare",
+            "method": "tx",
+            "params": serde_json::json!({
+                "tx_hash": CryptoHash::new().to_string(),
+                "sender_account_id": "frolik.near"
+            }),
+        });
+
+        let response = &mut client
+            .client
+            .post(&client.server_addr)
+            .insert_header(("Content-Type", "application/json"))
+            .send_json(&json)
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::REQUEST_TIMEOUT);
+    });
+}
+
+#[test]
+fn test_good_handler_error_status_code() {
+    test_with_client!(test_utils::NodeType::NonValidator, client, async move {
+        let json = serde_json::json!({
+            "jsonrpc": "2.0",
+            "id": "dontcare",
+            "method": "EXPERIMENTAL_receipt",
+            "params": serde_json::json!({"receipt_id": CryptoHash::new().to_string()})
+        });
+
+        let response = &mut client
+            .client
+            .post(&client.server_addr)
+            .insert_header(("Content-Type", "application/json"))
+            .send_json(&json)
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
     });
 }
 